c1dcabfdccf5198d04acef2312343283e032b67c
Strangejackets <koen-claes@live.be>
2016-07-08 15:39:21 +0200

    PSPingSweep

---
+++ Get-LiveHosts/Get-LiveHosts.psm1
@@ -0,0 +1,25 @@
+﻿<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Get-LiveHosts
+{
+
+    Begin
+    {$subnet = read-host "enter subnet to scan format '172.16.100.x'"
+    }
+    Process
+    {for ($i=1; $i -le 255; $i++) {
+    ping -n 1 -w 250 $subnet$i | select-string Packets:
+    nslookup $subnet$i}
+    }
+    End
+    {
+    }
+}
